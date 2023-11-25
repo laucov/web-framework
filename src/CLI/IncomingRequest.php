22094d34ba48465f4148ad9f14e84c789c39f74a
covaleski <covaleski23@gmail.com>
2023-11-25 12:00:21 +0000

    feat: create `IncomingRequest`

---
+++ src/CLI/IncomingRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Covaleski\Framework\CLI;
+
+/**
+ * Stores information about a CLI request.
+ */
+class IncomingRequest extends AbstractRequest
+{
+    /**
+     * Script handling the request.
+     */
+    protected string $filename;
+
+    /**
+     * Create the incoming request instance.
+     */
+    public function __construct(array $arguments)
+    {
+        foreach ($arguments as $i => $argument) {
+            // Check if the argument is a string.
+            if (!is_string($argument)) {
+                $message = 'All arguments must be of type string.';
+                throw new \InvalidArgumentException($message);
+            }
+            // Parse argument.
+            switch ($i) {
+                case 0:
+                    $this->filename = $argument;
+                    break;
+                case 1:
+                    $this->command = $argument;
+                    break;
+                default:
+                    $this->arguments[] = $argument;
+            }
+        }
+    }
+
+    /**
+     * Get the script handling the request.
+     */
+    public function getFilename(): ?string
+    {
+        return $this->filename ?? null;
+    }
+}
