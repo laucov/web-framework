ec2bb19ab7f0406c307a41445df36769109d00f2
covaleski <covaleski23@gmail.com>
2023-11-25 17:09:38 +0000

    test: `OutgoingRequest`

---
+++ tests/CLI/OutgoingRequestTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+use Covaleski\Framework\CLI\OutgoingRequest;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Covaleski\Framework\CLI\OutgoingRequest
+ */
+final class OutgoingRequestTest extends TestCase
+{
+    protected OutgoingRequest $request;
+
+    protected function setUp(): void
+    {
+        $this->request = new OutgoingRequest();
+    }
+
+    /**
+     * @covers ::setArguments
+     * @uses Covaleski\Framework\CLI\AbstractRequest::getArguments
+     */
+    public function testCanSetArguments(): void
+    {
+        $input = ['arg1', 'arg2', 'arg3'];
+        $this->request->setArguments($input);
+        
+        $output = $this->request->getArguments();
+        $this->assertSameSize($input, $output);
+        foreach ($output as $i => $argument) {
+            $this->assertSame($input[$i], $output[$i]);
+        }
+
+        $this->expectException(\InvalidArgumentException::class);
+        $this->request->setArguments(['arg1', 0, true]);
+    }
+
+    /**
+     * @covers ::setCommand
+     * @uses Covaleski\Framework\CLI\AbstractRequest::getCommand
+     */
+    public function testCanSetCommand(): void
+    {
+        $this->request->setCommand('do-something');
+        $this->assertSame('do-something', $this->request->getCommand());
+    }
+}
