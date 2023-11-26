eff78d8833c87edd55802b51fd941ef5f8015f0b
covaleski <covaleski23@gmail.com>
2023-11-26 19:27:52 +0000

    test: `Router`

--- tests/CLI/RouterTest.php
+++ tests/CLI/RouterTest.php
@@ -29,29 +29,38 @@ final class RouterTest extends TestCase
         $this->router->addCommand('invalid-class', stdClass::class);
     }
 
-    // /**
-    //  * @covers ::route
-    //  */
-    // public function testCanRoute(): void
-    // {
-    //     // Add example command.
-    //     $mock = $this
-    //         ->getMockBuilder(Command::class)
-    //         ->setMockClassName('RouterCommandTest')
-    //         ->getMockForAbstractClass();
-    //     $this->router->addCommand('test-the-router', $mock::class);
+    /**
+     * @covers ::route
+     * @covers ::getCommand
+     * @uses Covaleski\Framework\CLI\AbstractRequest::getCommand
+     * @uses Covaleski\Framework\CLI\OutgoingRequest::setArguments
+     * @uses Covaleski\Framework\CLI\OutgoingRequest::setCommand
+     * @uses Covaleski\Framework\CLI\Router::addCommand
+     */
+    public function testCanRoute(): void
+    {
+        // Add example command.
+        $mock = $this
+            ->getMockBuilder(Command::class)
+            ->setMockClassName('RouterCommandTest')
+            ->getMockForAbstractClass();
+        $this->router->addCommand('test-the-router', $mock::class);
 
-    //     // Get a request instance.
-    //     $request = new OutgoingRequest();
-    //     $request->setCommand('test-the-router');
-    //     $request->setArguments([]);
+        // Get a request instance.
+        $request = new OutgoingRequest();
 
-    //     // Get the command.
-    //     $command = $this->router->route($request);
-    //     $this->assertInstanceOf($mock::class, $command);
+        // Route with valid command.
+        $request->setCommand('test-the-router');
+        $request->setArguments([]);
+        $command = $this->router->route($request);
+        $this->assertInstanceOf($mock::class, $command);
 
-    //     // Get an inexistent command.
-    //     $request->setCommand('inexistent-command');
-    //     $this->assertNull($this->router->route($request));
-    // }
+        // Route with invalid command.
+        $request->setCommand('inexistent-command');
+        $this->assertNull($this->router->route($request));
+
+        // Route without command.
+        $this->expectException(\InvalidArgumentException::class);
+        $this->router->route(new OutgoingRequest());
+    }
 }
