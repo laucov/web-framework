8dee568627764265fde9d3a4fa9173b48716dd81
covaleski <covaleski23@gmail.com>
2023-12-04 19:54:13 +0000

    feat: add headers to `AbstractOutgoingMessage`

--- src/HTTP/AbstractOutgoingMessage.php
+++ src/HTTP/AbstractOutgoingMessage.php
@@ -11,6 +11,22 @@ use Covaleski\Framework\Files\StringSource;
 abstract class AbstractOutgoingMessage extends AbstractMessage
 {
     /**
+     * Add a message header.
+     * 
+     * If the header exists, appends the value, otherwise creates it.
+     */
+    public function addHeader(string $name, string $value): static
+    {
+        $list = $this->getHeaderAsList($name);
+
+        if ($list === null) {
+            return $this->setHeader($name, $value);
+        }
+
+        $list[] = trim($value);
+        return $this->setHeader($name, implode(', ', $list));
+    }
+    /**
      * Set the message body.
      * 
      * @param string|resource $content
@@ -21,11 +37,12 @@ abstract class AbstractOutgoingMessage extends AbstractMessage
         return $this;
     }
 
-    // /**
-    //  * Set a message header.
-    //  */
-    // public function setHeader(string $name, string $value): static
-    // {
-    //     return $this;
-    // }
+    /**
+     * Set a message header.
+     */
+    public function setHeader(string $name, string $value): static
+    {
+        $this->headers[$name] = trim($value);
+        return $this;
+    }
 }
