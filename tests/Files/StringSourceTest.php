940cb81c0910880bdf5c74b671fd01a6b2eda6f5
covaleski <covaleski23@gmail.com>
2023-12-04 13:18:09 +0000

    test: test `StringSource` with actual file

--- tests/Files/StringSourceTest.php
+++ tests/Files/StringSourceTest.php
@@ -16,6 +16,8 @@ final class StringSourceTest extends TestCase
 
     private StringSource $sourceB;
 
+    private StringSource $sourceC;
+
     private string $text;
 
     protected function setUp(): void
@@ -25,9 +27,11 @@ final class StringSourceTest extends TestCase
 
         // Get source from string.
         $this->sourceA = new StringSource($this->text);
-        // Get source from resource.
+        // Get source from custom resource.
         $resource = fopen('data://text/plain,' . $this->text, 'r');
         $this->sourceB = new StringSource($resource);
+        // Get source from file.
+        $this->sourceC = new StringSource(fopen(__FILE__, 'r'));
     }
 
     /**
@@ -39,29 +43,40 @@ final class StringSourceTest extends TestCase
      */
     public function testCanRead(): void
     {
+        // Set file resource expected data.
+        $contents = file_get_contents(__FILE__);
+        $filesize = filesize(__FILE__);
+
         // Get size.
         $size_a = $this->sourceA->getSize();
         $size_b = $this->sourceB->getSize();
+        $size_c = $this->sourceC->getSize();
         $this->assertSame(44, $size_a);
         $this->assertSame(44, $size_b);
+        $this->assertSame($filesize, $size_c);
         
         // Read all content.
         $this->assertSame($this->text, $this->sourceA->read($size_a));
         $this->assertSame($this->text, $this->sourceB->read($size_b));
+        $this->assertSame($contents, $this->sourceC->read($size_c));
 
         // Try reading after EOF.
         $this->assertSame('', $this->sourceA->read($size_a));
         $this->assertSame('', $this->sourceB->read($size_b));
+        $this->assertSame('', $this->sourceC->read($size_c));
 
         // Ensure pointer is at EOF.
         $this->assertSame(44, $this->sourceA->tell());
         $this->assertSame(44, $this->sourceB->tell());
+        $this->assertSame($filesize, $this->sourceC->tell());
 
         // Move pointer.
         $this->sourceA->seek(0);
         $this->sourceB->seek(0);
+        $this->sourceC->seek(0);
         $this->assertSame(0, $this->sourceA->tell());
         $this->assertSame(0, $this->sourceB->tell());
+        $this->assertSame(0, $this->sourceC->tell());
     }
 
     /**
@@ -77,18 +92,7 @@ final class StringSourceTest extends TestCase
      * @covers ::read
      * @uses Covaleski\Framework\Files\StringSource::__construct
      */
-    public function testMustReadValidFileLenght(): void
-    {
-        // Test using negative positions.
-        $this->expectException(\InvalidArgumentException::class);
-        $this->sourceB->read(-1);
-    }
-
-    /**
-     * @covers ::read
-     * @uses Covaleski\Framework\Files\StringSource::__construct
-     */
-    public function testMustReadValidStringLenght(): void
+    public function testMustReadPositiveLenghts(): void
     {
         // Test using negative positions.
         $this->expectException(\InvalidArgumentException::class);
