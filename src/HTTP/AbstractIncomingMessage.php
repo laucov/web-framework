8ae868f4afc4d2e46be371add7fca696f0f50016
covaleski <covaleski23@gmail.com>
2023-12-06 13:17:30 +0000

    chore: set headers in `AbstractIncomingMessage`

--- src/HTTP/AbstractIncomingMessage.php
+++ src/HTTP/AbstractIncomingMessage.php
@@ -9,8 +9,21 @@ use Covaleski\Framework\Files\StringSource;
  */
 class AbstractIncomingMessage extends AbstractMessage
 {
+    /**
+     * Create the incoming message instance.
+     */
     public function __construct(mixed $content, array $headers)
     {
+        // Set body.
         $this->body = new StringSource($content);
+
+        // Set headers.
+        foreach ($headers as $name => $value) {
+            if (!is_string($name) || !is_string($value)) {
+                $message = 'Header name and value must both be strings.';
+                throw new \InvalidArgumentException($message);
+            }
+            $this->headers[$name] = $value;
+        }
     }
 }
