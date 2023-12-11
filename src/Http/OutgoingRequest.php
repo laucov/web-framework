a2a7a2cb0f8cbfbd44f72a2e0f135da742635e56
covaleski <covaleski23@gmail.com>
2023-12-11 13:42:56 +0000

    feat: add parameter setter to `OutgoingRequest`

--- src/Http/OutgoingRequest.php
+++ src/Http/OutgoingRequest.php
@@ -22,6 +22,29 @@ class OutgoingRequest extends AbstractOutgoingMessage implements
     }
 
     /**
+     * Set a URI parameter.
+     */
+    public function setParameter(string $name, string|array $value): void
+    {
+        // Ensure parameter lists have only string elements.
+        if (is_array($value)) {
+            if (!array_is_list($value)) {
+                $message = 'Non-list array given as parameter list.';
+                throw new \InvalidArgumentException($message);
+            }
+            foreach ($value as $item) {
+                if (!is_string($item)) {
+                    $message = 'Parameter lists must only contain strings.';
+                    throw new \InvalidArgumentException($message);
+                }
+            }
+        }
+
+        // Set parameter.
+        $this->parameters[$name] = $value;
+    }
+
+    /**
      * Set the request URI.
      */
     public function setUri(Uri $uri): void
