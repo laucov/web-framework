240264a10bf28a4fd801a9ff312d5e173ca2cc29
covaleski <covaleski23@gmail.com>
2023-12-16 12:57:43 +0000

    feat: `ArrayBuilder` write functions

--- src/Data/ArrayBuilder.php
+++ src/Data/ArrayBuilder.php
@@ -59,6 +59,94 @@ class ArrayBuilder
             ? $array[$last_key]
             : $default_value;
     }
+
+    /**
+     * Check if a value exists.
+     */
+    public function hasValue(int|string|array $keys): bool
+    {
+        // Resolve single key.
+        if (!is_array($keys)) {
+            return array_key_exists($keys, $this->array);
+        }
+
+        // Check keys.
+        $keys = $this->validateKeys($keys);
+
+        // Get the last key.
+        $last_key = array_pop($keys);
+
+        // Find intermediary keys.
+        $array = &$this->array;
+        foreach ($keys as $key) {
+            if (!array_key_exists($key, $array) || !is_array($array[$key])) {
+                return false;
+            }
+            $array = &$array[$key];
+        }
+
+        return array_key_exists($last_key, $array);
+    }
+
+    /**
+     * Remove a value.
+     */
+    public function removeValue(int|string|array $keys): void
+    {
+        // Resolve single key.
+        if (!is_array($keys)) {
+            unset($this->array[$keys]);
+            return;
+        }
+
+        // Check keys.
+        $keys = $this->validateKeys($keys);
+
+        // Get the last key.
+        $last_key = array_pop($keys);
+
+        // Find/fill intermediary keys.
+        $array = &$this->array;
+        foreach ($keys as $key) {
+            if (!array_key_exists($key, $array) || !is_array($array[$key])) {
+                return;
+            }
+            $array = &$array[$key];
+        }
+
+        // Set value.
+        unset($array[$last_key]);
+    }
+
+    /**
+     * Set a value.
+     */
+    public function setValue(int|string|array $keys, mixed $value): void
+    {
+        // Resolve single key.
+        if (!is_array($keys)) {
+            $this->array[$keys] = $value;
+            return;
+        }
+
+        // Check keys.
+        $keys = $this->validateKeys($keys);
+
+        // Get the last key.
+        $last_key = array_pop($keys);
+
+        // Find/fill intermediary keys.
+        $array = &$this->array;
+        foreach ($keys as $key) {
+            if (!array_key_exists($key, $array) || !is_array($array[$key])) {
+                $array[$key] = [];
+            }
+            $array = &$array[$key];
+        }
+
+        // Set value.
+        $array[$last_key] = $value;
+    }
     
     /**
      * Validate a list of array keys.
