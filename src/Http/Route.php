3537bb162d8c7cc1254c8dedf1041d4d3b5c7843
covaleski <covaleski23@gmail.com>
2024-01-28 21:40:10 -0300

    feat: create `Route`

---
+++ src/Http/Route.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * This file is part of Laucov's Web Framework project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package web-framework
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+namespace Laucov\WebFramework\Http;
+
+/**
+ * Stores information about an HTTP route.
+ */
+class Route
+{
+    /**
+     * Found closure.
+     */
+    public null|\Closure $closure = null;
+
+    /**
+     * Captured parameters.
+     * 
+     * @var array<string>
+     */
+    protected array $parameters = [];
+
+    /**
+     * Add a parameter.
+     */
+    public function addParameter(string $parameter): void
+    {
+        $this->parameters[] = $parameter;
+    }
+
+    /**
+     * Get captured parameters.
+     * 
+     * @var array<string>
+     */
+    public function getParameters(): array
+    {
+        return $this->parameters;
+    }
+}
