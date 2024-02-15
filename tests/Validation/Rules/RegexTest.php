5a7424a53f0a79a78105b1a7fba35c31b4e42f50
covaleski <covaleski23@gmail.com>
2024-02-15 11:01:07 -0300

    feat: add `Regex` rule

---
+++ tests/Validation/Rules/RegexTest.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * This file is part of Laucov's Web Framework project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package web-framework
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Validation\Rules;
+
+use Laucov\WebFramework\Validation\Rules\Regex;
+use Tests\AbstractRuleTest;
+
+/**
+ * @coversDefaultClass \Laucov\WebFramework\Validation\Rules\Regex
+ */
+class RegexTest extends AbstractRuleTest
+{
+    protected string $className = Regex::class;
+
+    public function ruleProvider(): array
+    {
+        return [
+            [new Regex('/^\p{L}+$/'), [8]],
+            [new Regex('/\.com$/'), [2, 3]],
+            [new Regex('/^[A-Za-z\d\s\.]+$/'), [0, 1, 6, 7, 8]],
+            [new Regex('/^.+[@\?\.]+.+$/'), [2, 3, 4, 5]],
+        ];
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::validate
+     * @dataProvider ruleProvider
+     */
+    public function testCanValidate(Regex $rule, array $expected): void
+    {
+        $this->assertValidation($rule, $expected);
+    }
+
+    /**
+     * @covers ::validate
+     * @uses Laucov\WebFramework\Validation\Rules\Regex::__construct
+     */
+    public function testDoesNotAcceptNonScalarValues(): void
+    {
+        $this->assertRejectsNonScalarValues(new Regex('/^\d+$/'));
+    }
+
+    /**
+     * @coversNothing
+     */
+    public function testIsPropertyAttribute(): void
+    {
+        $this->assertIsPropertyAttribute(Regex::class);
+    }
+
+    protected function getValues(): array
+    {
+        return [
+            0 => 'The quick brown fox jumps over the lazy dog.',
+            1 => 'Lorem ipsum dolor',
+            2 => 'john.doe@foobar.com',
+            3 => 'michael.scott@dundermifflin.com',
+            4 => '<?php class Foo {}',
+            5 => '<?php class Bar extends Foo {}',
+            6 => '5063516945005047',
+            7 => '5063 5169 4500 5047',
+            8 => 'FooBarBaz'
+        ];
+    }
+}
