64fb960912d48c84047cc2966d1521824ca3fdce
covaleski <covaleski23@gmail.com>
2024-02-16 10:14:29 -0300

    feat: create `Collection`

---
+++ src/Modeling/Collection.php
@@ -0,0 +1,136 @@
+<?php
+
+/**
+ * This file is part of Laucov's Web Framework project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package web-framework
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+namespace Laucov\WebFramework\Modeling;
+
+/**
+ * Represents a list of database records.
+ * 
+ * @template T of Entity
+ */
+class Collection implements \Countable, \Iterator
+{
+    /**
+     * Stored entities.
+     * 
+     * @var array<T>
+     */
+    protected array $entities;
+
+    /**
+     * Current key.
+     */
+    protected int $key = 0;
+
+    /**
+     * Create the collection instance.
+     * 
+     * @param array<T> $entities
+     */
+    public function __construct(
+        /**
+         * Page which this collection represents.
+         */
+        public readonly int $page,
+
+        /**
+         * Maximum number of records shown by each page.
+         */
+        public readonly int $pageLength,
+
+        /**
+         * Number of records that match this collection's filter.
+         */
+        public readonly int $filteredCount,
+
+        /**
+         * Total number of records in the database table.
+         */
+        public readonly int $storedCount,
+
+        Entity ...$entities,
+    ) {
+        $this->entities = $entities;
+    }
+
+    /**
+     * Count entities in this collection.
+     * 
+     * The same result is obtained from passing this object to PHP's `count()`.
+     */
+    public function count(): int
+    {
+        return count($this->entities);
+    }
+
+    /**
+     * Returns the current entity.
+     * 
+     * @return T
+     */
+    public function current(): Entity
+    {
+        return $this->entities[$this->key];
+    }
+
+    /**
+     * Returns the index of the current entity.
+     */
+    public function key(): int
+    {
+        return $this->key;
+    }
+
+    /**
+     * Move forward to next entity.
+     * 
+     * Moves the current position to the next entity.
+     */
+    public function next(): void
+    {
+        $this->key++;
+    }
+
+    /**
+     * Rewind the Iterator to the first entity.
+     * 
+     * Rewinds back to the first entity of the Collection.
+     */
+    public function rewind(): void
+    {
+        $this->key = 0;
+    }
+
+    /**
+     * Checks if current position is valid.
+     */
+    public function valid(): bool
+    {
+        return array_key_exists($this->key, $this->entities);
+    }
+}
---
+++ tests/Modelling/CollectionTest.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * This file is part of Laucov's Web Framework project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package web-framework
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Modeling;
+
+use Laucov\WebFramework\Modeling\Collection;
+use Laucov\WebFramework\Modeling\Entity;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Laucov\WebFramework\Modeling\Collection
+ */
+class CollectionTest extends TestCase
+{
+    /**
+     * @covers ::__construct
+     * @covers ::count
+     * @covers ::current
+     * @covers ::key
+     * @covers ::next
+     * @covers ::rewind
+     * @covers ::valid
+     * @uses Laucov\WebFramework\Modeling\Entity::__construct
+     */
+    public function testCanInstantiate(): void
+    {
+        // Create collection.
+        $collection = new Collection(
+            2, 4, 7, 16,
+            $this->createProduct('ProdA', 23.99),
+            $this->createProduct('ProdB', 6.41),
+            $this->createProduct('ProdC', 1.99),
+        );
+
+        // Test properties.
+        $this->assertSame(2, $collection->page);
+        $this->assertSame(4, $collection->pageLength);
+        $this->assertSame(7, $collection->filteredCount);
+        $this->assertSame(16, $collection->storedCount);
+
+        // Test counting.
+        $this->assertSame(3, count($collection));
+
+        // Test iteration.
+        $actual = [];
+        foreach ($collection as $i => $entity) {
+            $actual[$i][] = $entity->code;
+            $actual[$i][] = $entity->price;
+        }
+        $expected = [['ProdA', 23.99], ['ProdB', 6.41], ['ProdC', 1.99]];
+        $this->assertSameSize($expected, $actual);
+        foreach ($expected as $i => $v) {
+            $this->assertSame($v[0], $actual[$i][0] ?? null);
+            $this->assertSame($v[1], $actual[$i][1] ?? null);
+        }
+    }
+
+    protected function createProduct(string $code, float $price): Product
+    {
+        $product = new Product();
+        $product->code = $code;
+        $product->price = $price;
+
+        return $product;
+    }
+}
+
+class Product extends Entity
+{
+    public string $code;
+    public float $price = 0.00;
+}
