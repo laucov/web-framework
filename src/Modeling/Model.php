4a1bd82f057f478216806ddd943d9347da0a8e1b
covaleski <covaleski23@gmail.com>
2024-02-19 11:12:24 -0300

    feat: create `Model`

---
+++ src/Modeling/Model.php
@@ -0,0 +1,431 @@
+<?php
+
+/**
+ * This file is part of Laucov's Web Framework project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package web-framework
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+namespace Laucov\WebFramework\Modeling;
+
+use Laucov\Db\Data\Connection;
+use Laucov\Db\Query\Table;
+
+/**
+ * Provides table data in form of entities and collections.
+ * 
+ * @template T of Entity
+ */
+abstract class Model
+{
+    /**
+     * Whether to reset the deletion filter upon the next query.
+     */
+    public bool $keepDeletionFilter = false;
+
+    /**
+     * Current deletion filter.
+     */
+    protected DeletionFilter $deletionFilter = DeletionFilter::HIDE;
+
+    /**
+     * Entity class name.
+     * 
+     * @var class-string<T>
+     */
+    protected string $entityName;
+
+    /**
+     * Selected page length.
+     */
+    protected null|int $pageLength = null;
+
+    /**
+     * Selected page number.
+     */
+    protected int $pageNumber = 1;
+
+    /**
+     * Primary key column.
+     */
+    protected string $primaryKey;
+
+    /**
+     * Sorting calls.
+     * 
+     * @var array<array{string, string}>
+     */
+    protected array $sorting = [];
+
+    /**
+     * Table instance.
+     */
+    protected Table $table;
+
+    /**
+     * Table name.
+     */
+    protected string $tableName;
+
+    /**
+     * Cached update values.
+     */
+    protected array $updateValues = [];
+
+    /**
+     * Create the model instance.
+     */
+    public function __construct(
+        /**
+         * Database connection interface.
+         */
+        protected Connection $connection,
+    ) {
+        // Store a table instance.
+        $this->table = new Table($connection, $this->tableName);
+    }
+
+    /**
+     * Perform a soft delete operation for the given primary key values.
+     */
+    public function delete(string ...$ids): void
+    {
+        $this->applyDeletionFilter();
+
+        $this->table
+            ->filter($this->primaryKey, '=', $ids)
+            ->updateRecords(['deleted_at' => date('Y-m-d H:i:s')]);
+    }
+
+    /**
+     * Permanently remove one or more records.
+     */
+    public function erase(string ...$ids): void
+    {
+        $this->applyDeletionFilter();
+
+        $this->table
+            ->filter($this->primaryKey, '=', $ids)
+            ->deleteRecords();
+    }
+
+    /**
+     * Check if a record exists.
+     */
+    public function exists(string ...$ids): bool
+    {
+        $this->applyDeletionFilter();
+
+        $count = $this->table
+            ->filter($this->primaryKey, '=', $ids)
+            ->countRecords($this->primaryKey);
+
+        return $count === count($ids);
+    }
+
+    /**
+     * Set how the model shall treat soft-deleted records.
+     */
+    public function filterDeleted(DeletionFilter $filter): static
+    {
+        $this->deletionFilter = $filter;
+        return $this;
+    }
+
+    /**
+     * Insert a new record.
+     * 
+     * @param T $entity
+     */
+    public function insert(mixed $entity): bool
+    {
+        // Validate entity.
+        if (!$entity->validate()) {
+            return false;
+        }
+
+        // Insert record.
+        $data = $entity->toArray();
+        $id = $this->table->insertRecord($data);
+        $entity->{$this->primaryKey} = $id;
+
+        return true;
+    }
+
+    /**
+     * Insert one or more records.
+     */
+    public function insertBatch(...$entities): bool
+    {
+        // Validate each entity.
+        $is_valid = true;
+        foreach ($entities as $entity) {
+            if (!$entity->validate() && $is_valid) {
+                $is_valid = false;
+            }
+        }
+        if (!$is_valid) {
+            return false;
+        }
+
+        // Insert all entities.
+        $data = array_map(fn ($e) => $e->toArray(), $entities);
+        $this->table->insertRecords(...$data);
+
+        return true;
+    }
+
+    /**
+     * List all records.
+     * 
+     * @return Collection<T>
+     */
+    public function listAll(): Collection
+    {
+        return $this->list();
+    }
+
+    /**
+     * Offset and limit the next list from a page length and number.
+     */
+    public function paginate(int $page_length, int $page_number): static
+    {
+        // Register pagination.
+        $this->pageLength = $page_length;
+        $this->pageNumber = $page_number;
+
+        return $this;
+    }
+
+    /**
+     * Find a single record by its primary key value.
+     * 
+     * @return T
+     */
+    public function retrieve(string $id): mixed
+    {
+        $this->applyDeletionFilter();
+
+        return $this->table
+            ->filter($this->primaryKey, '=', $id)
+            ->selectRecords($this->entityName)[0];
+    }
+
+    /**
+     * Find multiple records by their primary key value.
+     * 
+     * @return array<T>
+     */
+    public function retrieveBatch(string ...$ids): array
+    {
+        $this->applyDeletionFilter();
+
+        return $this->table
+            ->filter($this->primaryKey, '=', $ids)
+            ->selectRecords($this->entityName);
+    }
+
+    /**
+     * Sort the next list/retrieval.
+     */
+    public function sort(string $column_name, bool $descending = false): static
+    {
+        $this->table->sort($column_name, $descending);
+        return $this;
+    }
+
+    /**
+     * Update a record.
+     * 
+     * @param T $entity
+     */
+    public function update(mixed $entity): null|bool
+    {
+        // Validate entity.
+        if (!$entity->validate()) {
+            return false;
+        }
+
+        // Check if has entries.
+        $entries = $entity->getEntries();
+        if (ObjectReader::count($entries) < 1) {
+            return null;
+        }
+
+        // Insert record.
+        $this->table
+            ->filter($this->primaryKey, '=', $entity->{$this->primaryKey})
+            ->updateRecords((array) $entries);
+
+        return true;
+    }
+
+    /**
+     * Update multiple records with previously set values.
+     */
+    public function updateBatch(string ...$ids): BatchUpdateResult
+    {
+        // Check if values were set.
+        if (count($this->updateValues) < 1) {
+            $this->updateValues = [];
+            return BatchUpdateResult::NO_VALUES;
+        }
+
+        // Get records.
+        $records = $this->table
+            ->filter($this->primaryKey, '=', $ids)
+            ->selectRecords($this->entityName);
+        if (count($records) !== count($ids)) {
+            $this->updateValues = [];
+            return BatchUpdateResult::NOT_FOUND;
+        }
+
+        // Iterate records.
+        $ids = [];
+        foreach ($records as $record) {
+            // Set values.
+            foreach ($this->updateValues as $name => $value) {
+                $record->$name = $value;
+            }
+            // Validate.
+            if (!$record->validate()) {
+                $this->updateValues = [];
+                return BatchUpdateResult::INVALID_VALUES;
+            }
+            // Count entries.
+            // Ignore records that wouldn't change.
+            if (ObjectReader::count($record->getEntries()) > 0) {
+                $ids[] = (string) $record->{$this->primaryKey};
+            }
+        }
+
+        // Cancel if there is nothing to update.
+        if (count($ids) < 1) {
+            $this->updateValues = [];
+            return BatchUpdateResult::NO_ENTRIES;
+        }
+
+        // Update records.
+        $this->table
+            ->filter($this->primaryKey, '=', $ids)
+            ->updateRecords($this->updateValues);
+        
+        // Reset values.
+        $this->updateValues = [];
+
+        return BatchUpdateResult::SUCCESS;
+    }
+
+    /**
+     * Set a value for further batch update.
+     */
+    public function withValue(
+        string $column_name,
+        null|int|float|string $value,
+    ): static {
+        $this->updateValues[$column_name] = $value;
+        return $this;
+    }
+
+    /**
+     * Apply the current deletion filter to the table instance.
+     */
+    public function applyDeletionFilter(): void
+    {
+        // Set constraints according to the filter mode.
+        switch ($this->deletionFilter) {
+            case DeletionFilter::HIDE:
+                $this->table->filter('deleted_at', '=', null);
+                break;
+            case DeletionFilter::SHOW:
+                // Do nothing.
+                break;
+            case DeletionFilter::SHOW_EXCLUSIVELY:
+                $this->table->filter('deleted_at', '!=', null);
+                break;
+        }
+
+        // Reset the filter.
+        if (!$this->keepDeletionFilter) {
+            $this->deletionFilter = DeletionFilter::HIDE;
+        }
+    }
+
+    /**
+     * Get all filtered records as a `Collection`.
+     * 
+     * @return Collection<T>
+     */
+    protected function list(): Collection
+    {
+        // Apply deletion filter without resetting.
+        $previous_kdf_value = $this->keepDeletionFilter;
+        $this->keepDeletionFilter = true;
+        $this->applyDeletionFilter();
+        $this->keepDeletionFilter = $previous_kdf_value;
+
+        // Count all filtered records.
+        $this->table->autoReset = false;
+        $filtered = $this->table->countRecords($this->primaryKey);
+
+        // Paginate.
+        if ($this->pageLength !== null) {
+            $offset = abs($this->pageLength * ($this->pageNumber - 1));
+            $this->table
+                ->offset($offset)
+                ->limit($this->pageLength);
+        }
+        
+        // Get records.
+        $this->applyDeletionFilter();
+        $array = $this->table->selectRecords($this->entityName);
+        $this->table->reset();
+        $this->table->autoReset = true;
+
+        // Count total table records.
+        $stored = $this->table->countRecords($this->primaryKey);
+
+        // Create collection.
+        $collection = new Collection(
+            $this->pageNumber,
+            $this->pageLength,
+            $filtered,
+            $stored,
+            ...$array,
+        );
+
+        // Reset pagination.
+        $this->resetPagination();
+
+        return $collection;
+    }
+
+    /**
+     * Reset all cached collection metadata.
+     */
+    protected function resetPagination(): void
+    {
+        $this->pageLength = null;
+        $this->pageNumber = 1;
+    }
+}
---
+++ tests/Modelling/ModelTest.php
@@ -0,0 +1,425 @@
+<?php
+
+/**
+ * This file is part of Laucov's Web Framework project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package web-framework
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Modeling;
+
+use Laucov\Db\Data\Connection;
+use Laucov\Db\Data\Driver\DriverFactory;
+use Laucov\WebFramework\Modeling\BatchUpdateResult;
+use Laucov\WebFramework\Modeling\Collection;
+use Laucov\WebFramework\Modeling\DeletionFilter;
+use Laucov\WebFramework\Modeling\Entity;
+use Laucov\WebFramework\Modeling\Model;
+use Laucov\WebFramework\Validation\Rules\Regex;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Laucov\WebFramework\Modeling\Model
+ */
+class ModelTest extends TestCase
+{
+    protected Connection $conn;
+    protected AirplaneModel $model;
+
+    /**
+     * @covers ::applyDeletionFilter
+     * @covers ::delete
+     * @covers ::erase
+     * @covers ::exists
+     * @covers ::filterDeleted
+     * @uses Laucov\WebFramework\Modeling\Model::__construct
+     */
+    public function testCanDeleteAndErase(): void
+    {
+        // Delete single record.
+        $this->model->delete('1');
+        // Delete multiple records.
+        $this->model->delete('2', '3');
+        // Erase records.
+        $this->model
+            ->filterDeleted(DeletionFilter::SHOW)
+            ->erase('1');
+
+        // Turn off filter resetting for the next operations.
+        $this->model->keepDeletionFilter = true;
+
+        // Check active records.
+        $this->assertFalse($this->model->exists('999')); // Never existed
+        $this->assertFalse($this->model->exists('1')); // Erased
+        $this->assertFalse($this->model->exists('1', '2')); // Erased + Deleted
+        $this->assertFalse($this->model->exists('2', '3')); // Deleted
+        $this->assertFalse($this->model->exists('3', '4')); // Deleted + Active
+        $this->assertTrue($this->model->exists('4', '5')); // Active
+
+        // Check all records.
+        $this->model->filterDeleted(DeletionFilter::SHOW);
+        $this->assertFalse($this->model->exists('999'));
+        $this->assertFalse($this->model->exists('1'));
+        $this->assertFalse($this->model->exists('1', '2'));
+        $this->assertTrue($this->model->exists('2', '3'));
+        $this->assertTrue($this->model->exists('3', '4'));
+        $this->assertTrue($this->model->exists('4', '5'));
+
+        // Check deleted records.
+        $this->model->filterDeleted(DeletionFilter::SHOW_EXCLUSIVELY);
+        $this->assertFalse($this->model->exists('999'));
+        $this->assertFalse($this->model->exists('1'));
+        $this->assertFalse($this->model->exists('1', '2'));
+        $this->assertTrue($this->model->exists('2', '3'));
+        $this->assertFalse($this->model->exists('3', '4'));
+        $this->assertFalse($this->model->exists('4', '5'));
+
+        // Turn on filter resetting again.
+        $this->model->keepDeletionFilter = false;
+    }
+
+    /**
+     * @covers ::insert
+     * @covers ::insertBatch
+     * @covers ::update
+     * @covers ::updateBatch
+     * @covers ::withValue
+     * @uses Laucov\WebFramework\Modeling\Entity::__construct
+     * @uses Laucov\WebFramework\Modeling\Entity::__set
+     * @uses Laucov\WebFramework\Modeling\Entity::cacheRules
+     * @uses Laucov\WebFramework\Modeling\Entity::getEntries
+     * @uses Laucov\WebFramework\Modeling\Entity::getRuleset
+     * @uses Laucov\WebFramework\Modeling\Entity::toArray
+     * @uses Laucov\WebFramework\Modeling\Entity::validate
+     * @uses Laucov\WebFramework\Modeling\Model::__construct
+     * @uses Laucov\WebFramework\Modeling\Model::applyDeletionFilter
+     * @uses Laucov\WebFramework\Modeling\Model::retrieve
+     * @uses Laucov\WebFramework\Modeling\Model::retrieveBatch
+     * @uses Laucov\WebFramework\Modeling\ObjectReader::count
+     * @uses Laucov\WebFramework\Modeling\ObjectReader::diff
+     * @uses Laucov\WebFramework\Modeling\ObjectReader::toArray
+     * @uses Laucov\WebFramework\Validation\Rules\Regex::__construct
+     * @uses Laucov\WebFramework\Validation\Rules\Regex::validate
+     * @uses Laucov\WebFramework\Validation\Ruleset::addRule
+     * @uses Laucov\WebFramework\Validation\Ruleset::getErrors
+     * @uses Laucov\WebFramework\Validation\Ruleset::validate
+     */
+    public function testCanInsertAndUpdate(): void
+    {
+        // Create entities.
+        $airplane_a = new Airplane();
+        $airplane_a->registration = 'PR-AKA';
+        $airplane_a->manufacturer = 'ATR';
+        $airplane_a->model = '72-600';
+
+        // Insert single record.
+        $this->assertTrue($this->model->insert($airplane_a));
+        $this->assertSame(14, $airplane_a->id);
+
+        // Test validation.
+        $airplane_b = new Airplane();
+        $airplane_b->registration = 'PR-TOOLONG';
+        $airplane_b->manufacturer = 'Airbus';
+        $airplane_b->model = 'A320-214';
+        $this->assertFalse($this->model->insert($airplane_b));
+        $this->assertFalse(isset($airplane_b->id));
+        $airplane_b->registration = 'PR-MYR';
+        $this->assertTrue($this->model->insert($airplane_b));
+        $this->assertSame(15, $airplane_b->id);
+
+        // Test batch insert/validation.
+        $airplane_c = new Airplane();
+        $airplane_c->registration = 'LV-TOOLONG';
+        $airplane_c->manufacturer = 'Beech';
+        $airplane_c->model = 'King Air B200GT';
+        $airplane_d = new Airplane();
+        $airplane_d->registration = 'PS-GPA';
+        $airplane_d->manufacturer = 'Boeing';
+        $airplane_d->model = '737 MAX 8';
+        $this->assertFalse($this->model->insertBatch($airplane_c, $airplane_d));
+        $airplane_c->registration = 'LV-BMS';
+        $this->assertTrue($this->model->insertBatch($airplane_c, $airplane_d));
+        $this->assertSame('17', $this->conn->getLastId());
+        $this->assertFalse(isset($airplane_c->id));
+        $this->assertFalse(isset($airplane_d->id));
+
+        // Test updating.
+        $airplane_e = $this->model->retrieve('17');
+        $this->assertNull($this->model->update($airplane_e));
+        $airplane_e->registration = 'AA-AAAA';
+        $this->assertFalse($this->model->update($airplane_e));
+        $airplane_e->registration = 'AA-AAA';
+        $this->assertTrue($this->model->update($airplane_e));
+
+        // Update multiple.
+        $update = $this->model
+            ->withValue('model', 'A320-271N')
+            ->updateBatch('3', '12', '13');
+        $this->assertSame(BatchUpdateResult::SUCCESS, $update);
+        $records = $this->model->retrieveBatch('3', '12', '13');
+        foreach ($records as $record) {
+            $this->assertSame('A320-271N', $record->model);
+        }
+
+        // Test with invalid values.
+        $update = $this->model
+            ->withValue('registration', 'AB-CDEFG')
+            ->updateBatch('1', '2');
+        $this->assertSame(BatchUpdateResult::INVALID_VALUES, $update);
+
+        // Test with same values.
+        $update = $this->model
+            ->withValue('manufacturer', 'Boeing')
+            ->updateBatch('1', '5', '11');
+        $this->assertSame(BatchUpdateResult::NO_ENTRIES, $update);
+
+        // Test empty update.
+        $update = $this->model->updateBatch('3', '12', '13');
+        $this->assertSame(BatchUpdateResult::NO_VALUES, $update);
+
+        // Test with inexistent ID.
+        $update = $this->model
+            ->withValue('model', 'A320-271N')
+            ->updateBatch('3', '12', '56');
+        $this->assertSame(BatchUpdateResult::NOT_FOUND, $update);
+    }
+
+    /**
+     * @covers ::__construct
+     * @covers ::list
+     * @covers ::listAll
+     * @covers ::paginate
+     * @covers ::resetPagination
+     * @covers ::sort
+     * @uses Laucov\WebFramework\Modeling\Collection::__construct
+     * @uses Laucov\WebFramework\Modeling\Collection::count
+     * @uses Laucov\WebFramework\Modeling\Collection::current
+     * @uses Laucov\WebFramework\Modeling\Collection::get
+     * @uses Laucov\WebFramework\Modeling\Collection::next
+     * @uses Laucov\WebFramework\Modeling\Collection::rewind
+     * @uses Laucov\WebFramework\Modeling\Collection::valid
+     * @uses Laucov\WebFramework\Modeling\Entity::__construct
+     * @uses Laucov\WebFramework\Modeling\Entity::__set
+     * @uses Laucov\WebFramework\Modeling\Model::applyDeletionFilter
+     */
+    public function testCanList(): void
+    {
+        // List without pagination.
+        $records = $this->model->listAll();
+        $this->assertInstanceOf(Collection::class, $records);
+        $this->assertContainsOnlyInstancesOf(Airplane::class, $records);
+        $this->assertCount(13, $records);
+        $this->assertSame(1, $records->page);
+        $this->assertSame(null, $records->pageLength);
+        $this->assertSame(13, $records->filteredCount);
+        $this->assertSame(13, $records->storedCount);
+
+        // Add a record.
+        $this->conn->query(<<<SQL
+            INSERT INTO airplanes
+                (registration, manufacturer, model)
+            VALUES
+                ('PR-BCC', 'Learjet', '40')
+            SQL);
+
+        // Paginate - test without filters.
+        $records = $this->model
+            ->paginate(5, 3)
+            ->listAll();
+        $this->assertCount(4, $records);
+        $this->assertSame(3, $records->page);
+        $this->assertSame(5, $records->pageLength);
+        $this->assertSame(14, $records->filteredCount);
+        $this->assertSame(14, $records->storedCount);
+        $ids = [11, 12, 13, 14];
+        foreach ($ids as $i => $id) {
+            $this->assertSame($id, $records->get($i)->id);
+        }
+
+        // Paginate - test with filter.
+        $model = new class ($this->conn) extends AirplaneModel
+        {
+            /**
+             * List all planes for a specific manufacturer.
+             * 
+             * @return Collection<Airplane>
+             */
+            public function listForManufacturer(string $name): Collection
+            {
+                $this->table->filter('manufacturer', '=', $name);
+                return $this->list();
+            }
+        };
+        $records = $model
+            ->paginate(2, 3)
+            ->listForManufacturer('Airbus');
+        $this->assertCount(2, $records);
+        $this->assertSame(3, $records->page);
+        $this->assertSame(2, $records->pageLength);
+        $this->assertSame(6, $records->filteredCount);
+        $this->assertSame(14, $records->storedCount);
+
+        // Sort - ascending.
+        $collection = $this->model
+            ->sort('model')
+            ->paginate(3, 1)
+            ->listAll();
+        $this->assertSame(14, $collection->get(0)->id);
+        $this->assertSame(10, $collection->get(1)->id);
+        $this->assertSame(1, $collection->get(2)->id);
+
+        // Sort - descending.
+        $collection = $this->model
+            ->sort('registration', true)
+            ->paginate(2, 2)
+            ->listAll();
+        $this->assertSame(5, $collection->get(0)->id);
+        $this->assertSame(13, $collection->get(1)->id);
+    }
+
+    /**
+     * @covers ::sort
+     * @covers ::retrieve
+     * @covers ::retrieveBatch
+     * @uses Laucov\WebFramework\Modeling\Entity::__construct
+     * @uses Laucov\WebFramework\Modeling\Entity::__set
+     * @uses Laucov\WebFramework\Modeling\Model::__construct
+     * @uses Laucov\WebFramework\Modeling\Model::applyDeletionFilter
+     */
+    public function testCanRetrieve(): void
+    {
+        // Get single record.
+        $record = $this->model->retrieve('10');
+        $this->assertInstanceOf(Airplane::class, $record);
+        $this->assertSame('PP-PTM', $record->registration);
+        $this->assertSame('ATR', $record->manufacturer);
+        $this->assertSame('72-500', $record->model);
+
+        // Get multiple records.
+        $records = $this->model->retrieveBatch('9', '7');
+        $this->assertIsArray($records);
+        $this->assertContainsOnlyInstancesOf(Airplane::class, $records);
+        $this->assertCount(2, $records);
+        $this->assertSame('PT-VEV', $records[0]->registration);
+        $this->assertSame('Embraer', $records[0]->manufacturer);
+        $this->assertSame('EMB-820C Caraja', $records[0]->model);
+        $this->assertSame('PS-KLT', $records[1]->registration);
+        $this->assertSame('Piper', $records[1]->manufacturer);
+        $this->assertSame('PA-46-500TP', $records[1]->model);
+
+        // Sort and retrieve.
+        $records = $this->model
+            ->sort('manufacturer')
+            ->retrieveBatch('7', '10', '11');
+        $this->assertSame(10, $records[0]->id);
+        $this->assertSame(11, $records[1]->id);
+        $this->assertSame(7, $records[2]->id);
+        $records = $this->model
+            ->sort('id', true)
+            ->retrieveBatch('1', '10' , '5');
+        $this->assertSame(10, $records[0]->id);
+        $this->assertSame(5, $records[1]->id);
+        $this->assertSame(1, $records[2]->id);
+    }
+
+    /**
+     * @coversNothing
+     */
+    public function testFiltersSoftDeletedRecords(): void
+    {
+        // Create mock for model.
+        $model_mock = $this
+            ->getMockBuilder(AirplaneModel::class)
+            ->setConstructorArgs([$this->conn])
+            ->onlyMethods(['applyDeletionFilter'])
+            ->getMock();
+        $model_mock
+            ->expects($this->exactly(7))
+            ->method('applyDeletionFilter');
+        
+        // Test `AirplaneModel::applyDeletionFilter()`.
+        $model_mock->delete('3');
+        $model_mock->erase('9');
+        $model_mock->exists('1');
+        $model_mock->listAll();
+        $model_mock->retrieve('2');
+        $model_mock->retrieveBatch('1', '2');
+    }
+
+    protected function setUp(): void
+    {
+        // Create connection instance and table.
+        $this->conn = new Connection(new DriverFactory(), 'sqlite::memory:');
+        $this->conn
+            ->query(<<<SQL
+                CREATE TABLE airplanes (
+                    id INTEGER PRIMARY KEY,
+                    registration VARCHAR(8),
+                    manufacturer VARCHAR(32),
+                    model VARCHAR(128),
+                    deleted_at DATETIME
+                )
+                SQL)
+            ->query(<<<SQL
+                INSERT INTO airplanes
+                    (registration, manufacturer, model)
+                VALUES
+                    ('PR-XMI', 'Boeing', '737 MAX 8'),
+                    ('PR-XBR', 'Airbus', 'A320-271N'),
+                    ('PR-YRH', 'Airbus', 'A320-251N'),
+                    ('LV-KFX', 'Airbus', 'A320-232'),
+                    ('PS-GRC', 'Boeing', '737 MAX 8'),
+                    ('PR-XBF', 'Airbus', 'A320-273N'),
+                    ('PT-VEV', 'Embraer', 'EMB-820C Caraja'),
+                    ('PR-CPG', 'Cirrus', 'SR22'),
+                    ('PS-KLT', 'Piper', 'PA-46-500TP'),
+                    ('PP-PTM', 'ATR', '72-500'),
+                    ('LV-KEI', 'Boeing', '737 MAX 8'),
+                    ('CC-DBE', 'Airbus', 'A320-251N'),
+                    ('PR-YSH', 'Airbus', 'A320-251N')
+            SQL);
+        
+        $this->model = new AirplaneModel($this->conn);
+    }
+}
+
+/**
+ * @extends Model<Airplane>
+ */
+class AirplaneModel extends Model
+{
+    protected string $entityName = Airplane::class;
+    protected string $primaryKey = 'id';
+    protected string $tableName = 'airplanes';
+}
+
+class Airplane extends Entity
+{
+    public int $id;
+    #[Regex('/^[A-Z]{2}\-[A-Z]{3}$/')]
+    public string $registration;
+    public string $manufacturer;
+    public string $model;
+}
