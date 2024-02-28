2280388dc35a59d50c31adeece4689e0f1380a47
covaleski <covaleski23@gmail.com>
2024-02-28 12:44:55 -0300

    feat: create `ControllerRouter`

---
+++ src/Http/ControllerRouter.php
@@ -0,0 +1,206 @@
+<?php
+
+/**
+ * This file is part of Laucov's Web Framework project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package web-framework
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+namespace Laucov\WebFramework\Http;
+
+use Laucov\Http\Routing\Router;
+use Laucov\WebFramework\Providers\ConfigProvider;
+use Laucov\WebFramework\Providers\ServiceProvider;
+
+/**
+ * Routes controller requests.
+ */
+class ControllerRouter extends Router
+{
+    /**
+     * Current configuration provider in use.
+     */
+    protected ConfigProvider $configProvider;
+
+    /**
+     * Current controller class name.
+     * 
+     * @var class-string<AbstractController>
+     */
+    protected string $controllerName;
+
+    /**
+     * Active CRUD operations.
+     * 
+     * @var array<Crud>
+     */
+    protected array $crudOps = [
+        Crud::CREATE,
+        Crud::READ,
+        Crud::READ_ALL,
+        Crud::UPDATE,
+        Crud::DELETE,
+    ];
+
+    /**
+     * Current CRUD path options.
+     * 
+     * @var array<Crud, array{string, bool}>
+     */
+    protected array $crudPaths = [
+        'CREATE' => ['POST', 'create', false],
+        'READ' => ['GET', 'retrieve', true],
+        'READ_ALL' => ['GET', 'list', false],
+        'UPDATE' => ['PATCH', 'update', true],
+        'DELETE' => ['DELETE', 'delete', true],
+    ];
+
+    /**
+     * Whether to reset the CRUD operations list after the next CRUD routing.
+     */
+    protected bool $resetCrud = true;
+
+    /**
+     * Current service provider in use.
+     */
+    protected ServiceProvider $serviceProvider;
+
+    /**
+     * Exclude CRUD operations from the list in use.
+     */
+    public function resetCrudOps(): static
+    {
+        $this->crudOps = [
+            Crud::CREATE,
+            Crud::READ,
+            Crud::READ_ALL,
+            Crud::UPDATE,
+            Crud::DELETE,
+        ];
+
+        return $this;
+    }
+
+    /**
+     * Set the configuration of a CRUD path option.
+     */
+    public function setCrudPath(
+        Crud $operation,
+        string $http_method,
+        string $controller_method,
+        bool $has_id,
+    ): static {
+        // Set CRUD configuration.
+        $data = [$http_method, $controller_method, $has_id];
+        $this->crudPaths[$operation->name] = $data;
+
+        return $this;
+    }
+
+    /**
+     * Route CRUD methods for the current controller.
+     */
+    public function setCrudRoutes(string $path_pref, string $id_patt): static
+    {
+        foreach ($this->crudOps as $operation) {
+            [$method, $func_name, $has_id] = $this->crudPaths[$operation->name];
+            $path = $has_id ? (rtrim($path_pref) . "/{$id_patt}") : $path_pref;
+            $this->setMethodRoute($method, $path, $func_name);
+        }
+
+        if ($this->resetCrud) {
+            $this->resetCrudOps();
+        }
+
+        return $this;
+    }
+
+    /**
+     * Route one of the current controller methods.
+     */
+    public function setMethodRoute(
+        string $http_method,
+        string $path,
+        string $controller_method,
+    ): static {
+        $this->setClassRoute(
+            $http_method,
+            $path,
+            $this->controllerName,
+            $controller_method,
+            $this->configProvider,
+            $this->serviceProvider,
+        );
+        
+        return $this;
+    }
+
+    /**
+     * Set which CRUD operations should be routed from now on.
+     */
+    public function setCrudOps(Crud ...$operations): static
+    {
+        $this->crudOps = $operations;
+        $this->resetCrud = false;
+        return $this;
+    }
+
+    /**
+     * Set the current controller in use.
+     */
+    public function setController(string $class_name): static
+    {
+        if (!is_a($class_name, AbstractController::class, true)) {
+            $parent = AbstractController::class;
+            $message = "The passed class name does not extend {$parent}.";
+            throw new \InvalidArgumentException($message);
+        }
+
+        $this->controllerName = $class_name;
+
+        return $this;
+    }
+
+    /**
+     * Set the current providers in use.
+     */
+    public function setProviders(
+        ConfigProvider $config,
+        ServiceProvider $services,
+    ): static {
+        $this->configProvider = $config;
+        $this->serviceProvider = $services;
+
+        return $this;
+    }
+
+    /**
+     * Set which CRUD operations should be routed from now on.
+     */
+    public function withCrudOps(Crud ...$operations): static
+    {
+        $this->crudOps = $operations;
+        $this->resetCrud = true;
+        return $this;
+    }
+}
---
+++ tests/Http/ControllerRouterTest.php
@@ -0,0 +1,161 @@
+<?php
+
+/**
+ * This file is part of Laucov's Web Framework project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package web-framework
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Http;
+
+use Laucov\Http\Routing\Router;
+use Laucov\WebFramework\Http\AbstractController;
+use Laucov\WebFramework\Http\ControllerRouter;
+use Laucov\WebFramework\Http\Crud;
+use Laucov\WebFramework\Providers\ConfigProvider;
+use Laucov\WebFramework\Providers\ServiceProvider;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Laucov\WebFramework\Http\ControllerRouter
+ */
+class ControllerRouterTest extends TestCase
+{
+    /**
+     * @covers ::resetCrudOps
+     * @covers ::setController
+     * @covers ::setCrudOps
+     * @covers ::setCrudPath
+     * @covers ::setCrudRoutes
+     * @covers ::setMethodRoute
+     * @covers ::setProviders
+     * @covers ::withCrudOps
+     * @uses Laucov\WebFramework\Providers\ConfigProvider::__construct
+     * @uses Laucov\WebFramework\Providers\ServiceProvider::__construct
+     */
+    public function testCanRouteControllers(): void
+    {
+        // Create providers.
+        $c = new ConfigProvider([]);
+        $s = new ServiceProvider($c);
+
+        // Mock router.
+        $router = $this->getMockBuilder(ControllerRouter::class)
+            ->onlyMethods(['setClassRoute'])
+            ->getMock();
+        $router
+            ->expects($this->exactly(18))
+            ->method('setClassRoute')
+            ->withConsecutive(
+                ['GET', '/flights', A::class, 'list', $c, $s],
+                ['POST', 'names', B::class, 'create', $c, $s],
+                ['GET', 'names/:int', B::class, 'retrieve', $c, $s],
+                ['GET', 'names', B::class, 'list', $c, $s],
+                ['PATCH', 'names/:int', B::class, 'update', $c, $s],
+                ['DELETE', 'names/:int', B::class, 'delete', $c, $s],
+                ['GET', 'cars/:int', C::class, 'retrieve', $c, $s],
+                ['GET', 'cars', C::class, 'list', $c, $s],
+                ['PUT', 'cars', C::class, 'replace', $c, $s],
+                ['GET', 'animals', D::class, 'list', $c, $s],
+                ['POST', 'animals', D::class, 'create', $c, $s],
+                ['GET', 'foobars', E::class, 'list', $c, $s],
+                ['POST', 'foobars', E::class, 'create', $c, $s],
+                ['POST', 'fruits', F::class, 'create', $c, $s],
+                ['GET', 'fruits/:int', F::class, 'retrieve', $c, $s],
+                ['GET', 'fruits', F::class, 'list', $c, $s],
+                ['PUT', 'fruits', F::class, 'replace', $c, $s],
+                ['DELETE', 'fruits/:int', F::class, 'delete', $c, $s],
+            );
+
+        // Set routes.
+        $this->assertInstanceOf(Router::class, $router);
+        $router
+            ->setProviders($c, $s)
+            ->setController(A::class)
+                ->setMethodRoute('GET', '/flights', 'list')
+            ->setController(B::class)
+                ->setCrudRoutes('names', ':int')
+            ->setCrudPath(Crud::UPDATE, 'PUT', 'replace', false)
+            ->withCrudOps(Crud::READ, Crud::READ_ALL, Crud::UPDATE)
+            ->setController(C::class)
+                ->setCrudRoutes('cars', ':int')
+            ->setCrudOps(Crud::READ_ALL, Crud::CREATE)
+            ->setController(D::class)
+                ->setCrudRoutes('animals', ':int')
+            ->setController(E::class)
+                ->setCrudRoutes('foobars', ':int')
+            ->resetCrudOps()
+            ->setController(F::class)
+                ->setCrudRoutes('fruits', ':int');
+    }
+
+    /**
+     * @covers ::setController
+     * @uses Laucov\WebFramework\Http\ControllerRouter::setProviders
+     * @uses Laucov\WebFramework\Providers\ConfigProvider::__construct
+     * @uses Laucov\WebFramework\Providers\ServiceProvider::__construct
+     */
+    public function testControllerMustExtendAbstractController(): void
+    {
+        // Create providers.
+        $c = new ConfigProvider([]);
+        $s = new ServiceProvider($c);
+
+        // Create router.
+        $router = new ControllerRouter();
+        $router->setProviders($c, $s);
+
+        $this->expectException(\InvalidArgumentException::class);
+        $router->setController(G::class);
+    }
+}
+
+class A extends AbstractController
+{
+}
+
+class B extends AbstractController
+{
+}
+
+class C extends AbstractController
+{
+}
+
+class D extends AbstractController
+{
+}
+
+class E extends AbstractController
+{
+}
+
+class F extends AbstractController
+{
+}
+
+class G
+{
+}
